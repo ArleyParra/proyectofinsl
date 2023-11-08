9977ade498f12e68dc8fa31726b1c9b934bc9014
ya <aparra269@unab.edu.co>
2023-11-08 17:11:04 -0500

    Arreglo botones inicio y el piede de pagina

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -4,7 +4,7 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
   <a class="navbar-brand" href="#">
-      <img src="img/logo.png" alt="Bootstrap" width="30" height="24">
+      <img src="img/logo.png" alt="Bootstrap" width="60" height="60">
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -17,6 +17,15 @@
     <div class="col">
     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             ARRIENDO
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">APARTAMENTO</a></li>
+            <li><a class="dropdown-item" href="#">CASA</a></li>
+          </ul>
+    </div>
+    <div class="col">
+    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            VENTA
           </a>
           <ul class="dropdown-menu">
             <li><a class="dropdown-item" href="#">APARTAMENTO</a></li>
@@ -37,16 +46,16 @@
 <div id="carouselExample" class="carousel slide">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="img/contactenos.png" class="d-block w-100" alt="...">
+      <img src="img/portada_2.png" class="d-block w-100" alt="Bootstrap" width="1000" height="400">
     </div>
     <div class="carousel-item">
-      <img src="img/covid_info.png" class="d-block w-100" alt="...">
+      <img src="img/covid_info.png" class="d-block w-100" alt="Bootstrap" width="1000" height="400">
     </div>
     <div class="carousel-item">
-      <img src="img/portada.png" class="d-block w-100" alt="...">
+      <img src="img/portada.png" class="d-block w-100" alt="Bootstrap" width="1000" height="400">
     </div>
     <div class="carousel-item">
-      <img src="img/portada_2.png" class="d-block w-100" alt="...">
+      <img src="img/portada_2.png" class="d-block w-100" alt="Bootstrap" width="1000" height="400">
     </div>
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
@@ -59,13 +68,13 @@
   </button>
 </div>
 
-<h1 class="text-center m-5"> Arriendo Destacados </h1>
+<h1 class="text-center m-5"> Arriendos Destacados </h1>
 
 <div class="container text-center">
     <div class="row">
         <div class="col">
             <div class="card" style="width: 18rem;">
-                <img src="img/apto_alarcon.jpg" class="card-img-top" alt="...">
+                <img src="img/apto_alarcon.jpg" class="card-img-top" alt="Bootstrap" width="400" height="440">
                 <div class="card-body">
                     <h5 class="card-title">Apartaestudio</h5>
                     <p class="card-text">1 Habitacion</p>
@@ -76,7 +85,7 @@
         </div>
         <div class="col">
             <div class="card" style="width: 18rem;">
-                <img src="img/apto_cañaveral.jpg" class="card-img-top" alt="...">
+                <img src="img/apto_cañaveral.jpg" class="card-img-top" alt="Bootstrap" width="400" height="400">
                 <div class="card-body">
                     <h5 class="card-title">Apartamento</h5>
                     <p class="card-text">2 habitaciones</p>
@@ -88,7 +97,7 @@
         </div>
         <div class="col">
             <div class="card" style="width: 18rem;">
-                <img src="img/apto_cañaveral.jpg" class="card-img-top" alt="...">
+                <img src="img/apto_cañaveral.jpg" class="card-img-top" alt="Bootstrap" width="400" height="400">
                 <div class="card-body">
                     <h5 class="card-title">Apartamento</h5>
                     <p class="card-text">2 habitaciones</p>
@@ -105,7 +114,7 @@
     <div class="row">
         <div class="col">
             <div class="card" style="width: 18rem;">
-                <img src="img/casa_club_house.jpg" class="card-img-top" alt="...">
+                <img src="img/casa_club_house.jpg" class="card-img-top" alt="Bootstrap" width="400" height="440">
                 <div class="card-body">
                     <h5 class="card-title">CASA</h5>
                     <p class="card-text">4 Habitacion</p>
@@ -116,7 +125,7 @@
         </div>
         <div class="col">
             <div class="card" style="width: 18rem;">
-                <img src="img/apto_estudio_cañaveral.jpg" class="card-img-top" alt="...">
+                <img src="img/apto_estudio_cañaveral.jpg" class="card-img-top" alt="Bootstrap" width="400" height="400">
                 <div class="card-body">
                     <h5 class="card-title">Apartamento</h5>
                     <p class="card-text">3 habitaciones</p>
@@ -128,7 +137,7 @@
         </div>
         <div class="col">
             <div class="card" style="width: 18rem;">
-                <img src="img/apto_cañaveral.jpg" class="card-img-top" alt="...">
+                <img src="img/apto_cañaveral.jpg" class="card-img-top" alt="Bootstrap" width="400" height="400">
                 <div class="card-body">
                     <h5 class="card-title">Apartamento</h5>
                     <p class="card-text">2 habitaciones</p>
@@ -145,7 +154,7 @@
   <div class="container py-4 py-md-5 px-4 px-md-3 text-body-secondary">
     <div class="row">
       <div class="col-lg-3 mb-3">
-        <a class="d-inline-flex align-items-center mb-2 text-body-emphasis text-decoration-none" href="/" aria-label="Bootstrap">
+        <a class="d-inline-flex align-items-center mb-2 text-body-emphasis text-decoration-none" href="#" aria-label="Bootstrap">
           <span class="fs-5">INMOBILIARIA ROJAS VALBUENA</span>
         </a>
       
@@ -153,9 +162,9 @@
       <div class="col-6 col-lg-2 offset-lg-1 mb-3">
         <h5>ENCUENTRA</h5>
         <ul class="list-unstyled">
-          <li class="mb-2"><a href="/">ARRIENDO</a></li>
-          <li class="mb-2"><a href="/docs/5.3/">VENTA</a></li>
-          <li class="mb-2"><a href="https://icons.getbootstrap.com/">QUIENES SOMOS</a></li>
+          <li class="mb-2"><a href="#">ARRIENDO</a></li>
+          <li class="mb-2"><a href="#">VENTA</a></li>
+          <li class="mb-2"><a href="#">QUIENES SOMOS</a></li>
          </ul>
       </div>
 
